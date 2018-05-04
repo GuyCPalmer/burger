504f5cbd64df8362a841e8b22097409b39cfbb94
Guy Palmer <guypalmer@Guys-Air.attlocal.net>
2018-05-04 12:18:46 -0500

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Chease Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Veggie Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES("Bacon Cheeseburger", false);
