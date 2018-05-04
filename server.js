9a23424869e66cfee32a787bd97d5311d6793569
Guy Palmer <guypalmer@Guys-Air.attlocal.net>
2018-05-04 12:10:29 -0500

    server.js setup

--- server.js
+++ server.js
@@ -2,7 +2,36 @@
 //--------------------------
 const express = require ("express");
 const bodyParser = require ("body-parser");
+const methodOverride = require("method-override");
+const path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+var db = require ('./models');
+var router = require ("./controller/burgers_controller.js");
+
+//serve static content fo rthe app from the public directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+//set the handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// assign server to access the routes 
+app.use("/", routes);
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT" + PORT);
+    });
+});
+
+
